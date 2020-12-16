627592cd8f82ab63d283cdf2b25d557febdffe23
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-12-16 01:41:35 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+var express = require('express');
+const port = process.env.PORT || 3000;
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+
+app.get('/',function(req,res){
+    fetch('http://xkcd.com/info.0.json')
+    .then(response =>  response.json())
+    .then(data => {
+    //console.log('SUCCESS!' + data)
+    res.render('index',{data:data});
+    });
+    
+})
+
+app.listen(port,function(){
+    console.log('listening on '+port)
+});
